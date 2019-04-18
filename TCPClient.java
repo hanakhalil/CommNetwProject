c973e39acdd63ab51bec01967e06b439c184f7e3
hanakhalil <49515480+hanakhalil@users.noreply.github.com>
2019-04-18 20:44:23 +0200

    Add files via upload

--- TCPClient.java
+++ TCPClient.java
@@ -1,37 +1,53 @@
 package networks;
 
+import java.io.*;
+import java.net.*;
 
-	import java.io.*; 
-	import java.net.*; 
-	 public class TCPClient { 
+public class TCPClient {
 
-	    public static void main(String argv[]) throws Exception 
-	    { 
-	        String sentence; 
-	        String modifiedSentence; 
+	public static void main(String argv[]) throws Exception {
+		String sentence; //el beygelk ml console
+		String modifiedSentence; //bygelk mn server
+		String chat = "";  
+		
+			
+			BufferedReader inFromUser = new BufferedReader(
+					new InputStreamReader(System.in));
+			sentence = inFromUser.readLine(); //took info from user in console 
+			if (sentence.contentEquals("CONNECT")) { //user katab connect fl console
+				System.out.println("connection established");
+				Socket clientSocket = new Socket("127.0.0.1", 6789);// bta3 el server
 
-	        BufferedReader inFromUser = 
-	          new BufferedReader(new InputStreamReader(System.in)); 
+			DataOutputStream outToServer = new DataOutputStream(
+					clientSocket.getOutputStream());
+			BufferedReader inFromServer = new BufferedReader(
+					new InputStreamReader(clientSocket.getInputStream()));
 
-	        Socket clientSocket = new Socket("127.0.0.1", 6789); 
+			while (true) { //while connected
+				if(chat.contentEquals("Disconnect")){
+					clientSocket.close();
+				}
+				
+				if(inFromServer.ready()){ //
+					modifiedSentence = inFromServer.readLine();
+					
+					
 
-	        DataOutputStream outToServer = 
-	          new DataOutputStream(clientSocket.getOutputStream()); 
-	        BufferedReader inFromServer = 
-	                new BufferedReader(new
-	                InputStreamReader(clientSocket.getInputStream())); 
+					System.out.println("FROM SERVER: " + modifiedSentence);
+				}
+				if(inFromUser.ready()){  //USER ANDO HAGA YB3THA
+					chat=inFromUser.readLine();
+					outToServer.writeBytes(chat + '\n'); //will send to server chat
+				}
+				
+				
 
-	              sentence = inFromUser.readLine(); 
+				
 
-	              outToServer.writeBytes(sentence + '\n'); 
-	              modifiedSentence = inFromServer.readLine(); 
+			}
 
-	              System.out.println("FROM SERVER: " + modifiedSentence); 
+		}
 
-	              clientSocket.close(); 
-	                         
-	          } 
-	      
-	        
+	}
 
 }
--- TCPServer.java
+++ TCPServer.java
@@ -10,26 +10,54 @@ public class TCPServer {
     { 
       String clientSentence; 
       String capitalizedSentence; 
+      String chat2="";
+      String sentence2 ="";
 
       ServerSocket welcomeSocket = new ServerSocket(6789); 
-  
-      while(true) { 
-  
-            Socket connectionSocket = welcomeSocket.accept(); 
-
-           BufferedReader inFromClient = 
+        
+        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(
+				System.in)); //allow eny aktb f console server
+        sentence2=inFromServer.readLine();
+        if (sentence2.contentEquals("CONNECT")) {
+			System.out.println("connection established");
+             
+              
+        Socket connectionSocket = welcomeSocket.accept(); //wafe2 al connection
+      
+      BufferedReader inFromClient = 
               new BufferedReader(new
-              InputStreamReader(connectionSocket.getInputStream())); 
+              InputStreamReader(connectionSocket.getInputStream())); //kalam gy ml client 
            DataOutputStream  outToClient = 
-                   new DataOutputStream(connectionSocket.getOutputStream()); 
+                   new DataOutputStream(connectionSocket.getOutputStream()); //el b3to lel client
+          
+           
+      
 
-                 clientSentence = inFromClient.readLine(); 
+      while(true) { 
+    	  if(inFromServer.ready()){ //kateb f conxole server haga
+				chat2=inFromServer.readLine();
+				outToClient.writeBytes(chat2 + '\n');
+			}
+    	  if(inFromClient.ready()){ //gyly haga ml client
+    		  clientSentence = inFromClient.readLine(); //el gylo ml client
 
-                 capitalizedSentence = clientSentence.toUpperCase() + '\n'; 
+              capitalizedSentence = clientSentence.toUpperCase() + '\n'; 
 
-                 outToClient.writeBytes(capitalizedSentence); 
+              System.out.println("USER:"+capitalizedSentence );
+    	  
+    	 
+				
+
+				
+			}
+            
+    	
+           
               } 
 
 
-    }}
+    }
+    }
+}
+
 
